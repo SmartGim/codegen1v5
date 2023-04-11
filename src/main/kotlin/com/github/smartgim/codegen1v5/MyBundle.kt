a2d4ab367afe0a11979b78c3201d7a3cf079a6f6
GitHub Action <action@github.com>
2023-04-11 00:55:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/smartgim/codegen1v5/MyBundle.kt
+++ src/main/kotlin/com/github/smartgim/codegen1v5/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.smartgim.codegen1v5
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/smartgim/codegen1v5/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smartgim/codegen1v5/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.smartgim.codegen1v5.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smartgim.codegen1v5.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
